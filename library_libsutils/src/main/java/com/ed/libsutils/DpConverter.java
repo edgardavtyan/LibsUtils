680b5e8e4b14ff8e3b455e2628b419ef5d654d67
Edgar Davtyan <edgar.davtyan@outlook.com>
2018-01-16 12:10:35 +0200

    Shorten DpConverter method names

--- library_libsutils/src/main/java/com/ed/libsutils/DpConverter.java
+++ library_libsutils/src/main/java/com/ed/libsutils/DpConverter.java
@@ -4,9 +4,15 @@ import android.content.res.Resources;
 import android.util.DisplayMetrics;
 
 public class DpConverter {
-	public static int convertDpToPixel(int dp) {
+	public static int dpToPixel(int dp) {
 		DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
 		int px = (int) (dp * ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT));
 		return px;
+	}
+
+	public static int pixelToDp(int px) {
+		DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+		int dp = (int) (px / ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT));
+		return dp;
 	}
 }
