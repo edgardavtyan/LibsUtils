36693119998c1179f46dc2fd9d763d13d0c9ce89
Edgar Davtyan <edgar.davtyan@outlook.com>
2018-01-28 20:44:14 +0200

    Add listeners with manually selectable methods

---
+++ app/src/main/java/com/ed/libsutils/listeners/SelectableAnimatorListener.java
@@ -0,0 +1,25 @@
+package com.ed.libsutils.listeners;
+
+import android.animation.Animator;
+
+public class SelectableAnimatorListener implements Animator.AnimatorListener {
+	@Override
+	public void onAnimationStart(Animator animation) {
+
+	}
+
+	@Override
+	public void onAnimationEnd(Animator animation) {
+
+	}
+
+	@Override
+	public void onAnimationCancel(Animator animation) {
+
+	}
+
+	@Override
+	public void onAnimationRepeat(Animator animation) {
+
+	}
+}
